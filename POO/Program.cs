2d95f9eb7c3b312322e08927c4f32273169be1cf
jossigaleano12 <132154112+jossigaleano12@users.noreply.github.com>
2024-02-16 17:00:48 -0600

    Add files via upload

---
+++ POO/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Alumno
+{
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+
+    public void ImprimirDetalles()
+    {
+        Console.WriteLine("Nombre: " + Nombre);
+        Console.WriteLine("Edad: " + Edad);
+    }
+}
+
+public class Registro
+{
+    static void Main(string[] args)
+    {
+        // Crear instancias de la clase Persona
+        Alumno Alumno1 = new Alumno();
+        Alumno1.Nombre = "Maria";
+        Alumno1.Edad = 15;
+
+        Alumno Alumno2 = new Alumno();
+        Alumno2.Nombre = "Luis";
+        Alumno2.Edad = 14;
+
+        Alumno Alumno3=new Alumno();
+        Alumno3.Nombre = "Daniela";
+        Alumno3.Edad = 17;
+
+        // Imprimir los detalles de las personas
+        Alumno1.ImprimirDetalles();
+        Console.WriteLine();
+
+        Alumno2.ImprimirDetalles();
+        Console.WriteLine();
+
+        Alumno3.ImprimirDetalles();
+        Console.WriteLine();
+    }
+}
