328452a6700a45c636714e35852fd18055a470bc
jossigaleano12 <132154112+jossigaleano12@users.noreply.github.com>
2024-03-02 11:07:30 -0600

    Add files via upload

---
+++ Primer Parcial/2Programa/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Programa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            double numero;
+            double suma = 0;
+            int numeros = 0;
+
+            Console.WriteLine("Ingrese números (ingrese 0 para terminar):");
+
+            do
+            {
+             Console.Write("Ingrese un número: ");
+             numero = double.Parse(Console.ReadLine());
+
+              if (numero != 0)
+              {
+               suma += numero;
+               numeros++;
+              }
+            } while (numero != 0);
+
+            if (numeros > 0)
+            {
+                double promedio = suma / numeros;
+                Console.WriteLine("La suma de los números ingresados es: " + suma);
+                Console.WriteLine("El promedio de los números ingresados es: " + promedio);
+            }
+            else
+            {
+             Console.WriteLine("No se ingresaron números");
+            }
+            
+        }
+    }
+    
+}
---
+++ Primer Parcial/Cajero/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                int opcion;
+                double saldo = 1000;
+                double cantidad;
+
+                Console.WriteLine("Bienvenido al Cajero Automático");
+                Console.WriteLine("1. Verificar saldo");
+                Console.WriteLine("2. Depositar");
+                Console.WriteLine("3. Retirar");
+                Console.WriteLine("4. Salir");
+
+                do
+                {
+                    Console.Write("Seleccione una opción: ");
+                    opcion = int.Parse(Console.ReadLine());
+
+                    switch (opcion)
+                    {
+                        case 1:
+                            Console.WriteLine("Su saldo es: " + saldo);
+                            break;
+                        case 2:
+                            Console.Write("Ingrese la cantidad a depositar: ");
+                            cantidad = double.Parse(Console.ReadLine());
+                            saldo += cantidad;
+                            Console.WriteLine("Depósito realizado con éxito");
+                            break;
+                        case 3:
+                            Console.Write("Ingrese la cantidad a retirar: ");
+                            cantidad = double.Parse(Console.ReadLine());
+                            if (cantidad > saldo)
+                            {
+                                Console.WriteLine("No tiene suficiente saldo para retirar esta cantidad");
+                            }
+                            else
+                            {
+                                saldo -= cantidad;
+                                Console.WriteLine("Retiro realizado con éxito");
+                            }
+                            break;
+                        case 4:
+                            Console.WriteLine("Saliendo del Cajero Automático");
+                            break;
+                        default:
+                            Console.WriteLine("Opción no válida");
+                            break;
+                    }
+                }   while (opcion != 4);
+        }
+    }
+
+}
