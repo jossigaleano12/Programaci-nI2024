2269ae3c46a2c4e90bf97055eb8ad546d95627ac
jossigaleano12 <132154112+jossigaleano12@users.noreply.github.com>
2024-03-22 20:49:01 -0600

    Add files via upload

---
+++ Semana7/AplicaciondeListas/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicaciondeListas
+{
+    internal class Program
+    {
+        static List<string> tareas = new List<string>();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Menú:");
+                Console.WriteLine("1. Mostrar tareas");
+                Console.WriteLine("2. Agregar tareas");
+                Console.WriteLine("3. Eliminar tareas");
+                Console.WriteLine("4.Salir");
+
+                Console.Write("Ingresa lo que quieres realizar: ");
+                int opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1: 
+                        Mostrartareas();
+                        break;
+                    case 2:
+                        Agregartareas();
+                        break;
+                    case 3:
+                        Eliminartareas();
+                        break;
+                    case 4:
+                        Console.WriteLine("Saliste del programa");
+                        return;
+                    default:
+                        Console.WriteLine("Seleccion invalida.");
+                        break;
+                }
+            }
+        }
+
+        static void Mostrartareas()
+        {
+            Console.WriteLine("Tareas:");
+            for (int i = 0; i < tareas.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {tareas[i]}");
+            }
+        }
+
+        static void Agregartareas()
+        {
+            Console.Write("Agrega tu tarea: ");
+            string tarea = Console.ReadLine();
+
+            tareas.Add(tarea);
+
+            Console.WriteLine("Haz agregado una tarea nueva!");
+        }
+
+        static void Eliminartareas()
+        {
+            Console.WriteLine("Que numero de tarea deseas eliminar?: ");
+            int tarea = Convert.ToInt32(Console.ReadLine());
+
+            if (tarea < 1 || tarea > tareas.Count)
+            {
+                Console.WriteLine("tarea invalido.");
+                return;
+            }
+
+            tareas.RemoveAt(tarea - 1);
+
+            Console.WriteLine("Tu tarea a sido eliminada!");
+        }
+    }
+}
---
+++ Semana7/Calculadoradedescuento/CalculadoraDescuento.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana7
+{
+    internal class CalculadoraDeDescuento
+    {
+        static void DescuentoAplicado(double[,] Compras)
+        {
+            for (int Cliente = 0; Cliente < 5; Cliente++)
+            {
+                double total = 0;
+                for (int compra = 0; compra < 5; compra++)
+                {
+                    total += Compras[Cliente, compra];
+                }
+
+                double descuento = 0;
+
+                if (total > 1000)
+                {
+                    descuento = total * 0.2;
+                }
+                else if (total > 100)
+                {
+                    descuento = total * 0.1;
+                }
+
+                Console.WriteLine($"Cliente {Cliente + 1} total: {total} - Descuento: {descuento} - Total a pagar: {total - descuento}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            double[,] Compras = new double[5, 5]
+                                {
+                                 { 121, 155, 181, 195, 231 },
+                                 { 122, 154, 182, 194, 232 },
+                                 { 123, 153, 183, 193, 233 },
+                                 { 124, 152, 184, 192, 234 },
+                                 { 125, 151, 185, 191, 235 }
+                                };
+
+            DescuentoAplicado(Compras);
+        }
+    }
+}
+
