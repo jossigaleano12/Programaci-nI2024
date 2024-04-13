e0b80c95b03506dc757183faf27282770b3f8cf5
jossigaleano12 <132154112+jossigaleano12@users.noreply.github.com>
2024-04-12 22:03:01 -0600

    Add files via upload

---
+++ CorreoElectronico/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CorreoElectronico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Este es mi correo electrónico: josselinegaleano@gmail.com. " +
+                "Cualquier duda tambien puede escribir al siguiente correo: Jossgaleano12@gmail.com." + 
+                "Para una respuesta mas rapida puedes escribirme a: jgaleanoh@miumg.edu.gt";
+
+            Regex emailRegex = new Regex(@"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b");
+
+            MatchCollection matches = emailRegex.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
